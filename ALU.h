9b14a5541d1ef4a3c2d50156328a1bb2eea1c1cc
Nícolas Estanislau <nicolas.estanislau@gmail.com>
2021-08-18 19:46:25 -0300

    ALU

---
+++ ALU.h
@@ -0,0 +1,21 @@
+#ifndef __ALU_H__
+#define __ALU_H__
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+#include "FuncoesAuxiliares.h"
+
+class ALU{
+    private:
+        bool zero;
+
+    public:
+        
+
+}
+
+
+#endif // __ALU_H__
